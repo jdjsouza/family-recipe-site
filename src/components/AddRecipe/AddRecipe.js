435cbd8e00f829d5485a0cc411966d23be6dea0c
xarsin <jdj.souza@gmail.com>
2020-12-03 19:44:08 -0600

    finally got the drop down box to populate

--- src/components/AddRecipe/AddRecipe.js
+++ src/components/AddRecipe/AddRecipe.js
@@ -40,30 +40,40 @@ class AddRecipe extends Component {
   };
 
   render() {
-    let units;
-    if (this.props.store.theUnits === undefined) {
-      units = <option value="loading">Loading...</option>;
-    } else {
-      units = this.props.store.theUnits.map((option) => {
-        <option key={option.id} value={option.id}>
-          {option.unit}
-        </option>;
-      });
-    }
+    // let units;
+    // if (this.props.store.theUnits === undefined) {
+    //   units = <option value="loading">Loading...</option>;
+    // } else {
+    //   units = this.props.store.theUnits.map((item, index) => {
+    //     <option key={index} value={item.id}>
+    //       {item.unit}
+    //     </option>;
+    //   });
+    // }
     return (
       <div>
         <h2 style={{ textAlign: 'center' }}>Add a new Recipe!</h2>
         <div>
-          <input placeholder="Recipe Name" type="text" />
+          <Grid container spacing={1} direction="row" justify="center">
+            <Grid item>
+              <input placeholder="Recipe Name" type="text" />
+            </Grid>
+          </Grid>
           <div>
-            <input placeholder="Ingredient" type="text" />
-            <input className="input-qty" placeholder="QTY" type="number" />
-            <select name="Measurement" onChange={this.handleSelect}>
-              <option key="0" value="0">
-                Measurements
-              </option>
-              {units}
-            </select>
+            <Grid container spacing={1} direction="row" justify="center">
+              <Grid item>
+                <input placeholder="Ingredient" type="text" />
+                <input className="input-qty" placeholder="QTY" type="number" />
+                <select name="Measurement" onChange={this.handleSelect}>
+                  <option value="">Measurement</option>
+                  {this.props.store.theUnits.map((item, index) => (
+                    <option key={index} value={item.id}>
+                      {item.unit}
+                    </option>
+                  ))}
+                </select>
+              </Grid>
+            </Grid>
           </div>
           {/* BUTTON TO ADD MORE INGREDIENT LINES */}
         </div>
